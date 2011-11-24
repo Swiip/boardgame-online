058b5f3a56171ed0a1d45d6050244b65f9399e52
Swiip <matthieu.lux@gmail.com>
2011-11-24 12:40:59 +0000

    pan fonctionne

--- public/scripts/controls.js
+++ public/scripts/controls.js
@@ -1,7 +1,37 @@
 define(["jquery", "jquery-ui-1.8.16.custom.min"], function($) {
     return {
         init: function() {
-            $(".board").draggable();
+            var controls = require("controls");
+            controls.board();
+            controls.pieces();
+        },
+        
+        mousedownEvent: null,
+        
+        board: function() {
+            var controls = require("controls");
+            $("html").mousedown(function(event) {
+                controls.mousedownEvent = event;
+            }).mousemove(function(event) {
+                if(controls.mousedownEvent !== null) {
+                    var position = $(".anchor").offset();
+                    $(".anchor").css({
+                        top : position.top + event.pageY - controls.mousedownEvent.pageY ,
+                        left : position.left + event.pageX - controls.mousedownEvent.pageX
+                    });
+                    controls.mousedownEvent = event;
+                }
+            }).mouseup(controls.dragFinish).mouseout(controls.dragFinish);
+        },
+        
+        dragFinish: function() {
+            require("controls").mousedownEvent = null;
+        },
+        
+        pieces: function() {
+            $(".pieces").draggable().mousedown(function() {
+                return false;
+            });
         }
     };
 });
--- public/scripts/loader.js
+++ public/scripts/loader.js
@@ -3,9 +3,19 @@ define(["jquery"], function($) {
         load: function(callback) {
             var game = "got9";
             $.getJSON(game + "/" + game + ".json", function(data) {
-                $.each(data.board, function(index, item){
-                    $("<img class='board' src='" + game + "/" + item.src + "' style='position: absolute; left: 0; top: 0;'/>").appendTo("body");
+                $.each(data.pieces, function(index, item){
+                    $("<div style='width: 0; height: 0;'><img class='pieces' src='" + game + "/" + item.src + "' style='position: relative; top: " + item.x + "px; left: " + item.y + "px; z-index: 5;'/></div>").appendTo(".anchor");
                 });
+                
+                $.each(data.board, function(index, item){
+                    $("<div style='width: 0; height: 0;'><img class='board' src='" + game + "/" + item.src + "' style='position: relative; top: " + item.x + "px; left: " + item.y + "px; z-index: 3;'/></div>").mousedown(function(event) {
+                        if(event.preventDefault) {
+                            event.preventDefault();
+                        }
+                    }).appendTo(".anchor");
+                });
+                
+                
                 callback();
             });
         }
